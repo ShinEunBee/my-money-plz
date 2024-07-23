f644c915fbc7955f0f87d6556773aad3c324763e
GGang <eun1018bee@naver.com>
2024-07-23 22:31:17 +0900

    CustomHook 연습

--- src/pages/CreateView.tsx
+++ src/pages/CreateView.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useFormInput } from "../hooks/useFormInput";
 
 export default function CreateView() {
-    return (
-        <>
-        <h1>저는 입력 화면 입니다.</h1>
-        </>
-    )
+  const myName = useFormInput("");
+  const yourName = useFormInput("");
+
+  return (
+    <>
+      <h1>저는 입력 화면 입니다.</h1>
+      <input type="text" {...myName} />
+      <br />
+      <input type="text" {...yourName} />
+      <br />
+      <h4>
+        {myName.value} 와 {yourName.value}
+      </h4>
+    </>
+  );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useFormInput } from "../hooks/useFormInput";
 
 export default function Main() {
-    return (
-        <>
-        <h1>저는 메인 입니다.</h1>
-        </>
-    )
+  const firstNameProps = useFormInput("음북");
+  const lastNameProps = useFormInput("신");
+
+  return (
+    <>
+      <label>
+        First name:
+        <input {...firstNameProps} />
+      </label>
+      <label>
+        Last name:
+        <input {...lastNameProps} />
+      </label>
+      <p>
+        <b>
+          Good morning, {firstNameProps.value} {lastNameProps.value}.
+        </b>
+      </p>
+    </>
+  );
 }
