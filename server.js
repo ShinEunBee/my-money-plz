28877ed26302aceef1fa64b2cce0b7a4832063bf
GGang <eun1018bee@naver.com>
2024-07-30 23:08:55 +0900

    index.ts 수정

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const app = express();
 const server = require("http").createServer(app);
 
-
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://<un>:<pw>@cluster0.lcuhmx9.mongodb.net/')
 .then(()=>console.log('MongoDB 가 연결'))
